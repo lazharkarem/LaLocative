0c4d440e68cd4ce21ef053857d9194d837de985e
lazharkarem <karem.lazhar@gmail.com>
2021-08-04 18:31:18 +0100

    Clean up

--- App.js
+++ App.js
@@ -6,73 +6,15 @@
  * @format
  * @flow strict-local
  */
-import {NavigationContainer} from '@react-navigation/native';
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {Component} from 'react';
+import MyStack from './navigation/stack';
 
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {
-  Text,
-  View,
-  Button,
-} from 'react-native';
-const HomeScreen = ({navigation}) => {
-  return (
-    // eslint-disable-next-line react-native/no-inline-styles
-    <View style={{flex: 1,alignItems:'center',justifyContent:'center'}}>
-      <Text>Home Screen</Text>
-      <Button title="Go To Details Screen"
-            onPress={() => navigation.navigate('Details')}
-      />
-    </View>
-  );
-};
+export default class App extends Component {
 
-const DetailsScreen = ({navigation}) => {
-  return (
-    // eslint-disable-next-line react-native/no-inline-styles
-    <View style={{flex: 1, alignItems:'center',justifyContent:'center'}}>
-      <Text>Details Screen</Text>
-      <Button title="Go To Details Screen .. again"
-            onPress={() => navigation.push('Details')}
-      />
-            <Button title="Go To Home Screen"
-            onPress={() => navigation.navigate('Home')}
-      />
-            <Button title="Go back"
-            onPress={() => navigation.goBack()}
-      />
-            <Button title="Go back to the 1st"
-            onPress={() => navigation.popToTop()}
-      />
-    </View>
-  );
-};
-
-const Stack = createNativeStackNavigator();
-
-
-const App = () => {
-
-  return (
-<NavigationContainer>
-      <Stack.Navigator screenOptions={{
-                  headerStyle:{
-                    backgroundColor:'#009387',
-                  },
-                  headerTintColor:'#fff',
-                  headerTitleStyle:{
-                    fontWeight:'bold',
-                  },
-      }}>
-        <Stack.Screen name="Home" component={HomeScreen} options={{
-          title:'Overview',
-
-        }}
-        />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>  );
-};
-
-export default App;
+  render() {
+      return (
+              <MyStack />
+      );
+  }
+}
